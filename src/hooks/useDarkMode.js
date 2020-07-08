b9a50f50fc9fdeb2d493b8098cb5a5fef0981fe0
Nic Lehman <niclehman@Nics-MacBook-Pro.local>
2020-07-08 15:05:35 -0500

    ➕ ADDED: useDarkMode hook

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,14 @@
+import { useEffect } from 'react';
+import useLocalStorage from './useLocalStorage';
+
+export const useForm = (key, initialValue) => {
+  // capture useLocalStorage return values in a const
+  const [darkMode, setDarkMode] = useLocalStorage('dark-mode', false);
+
+  // toggle 'dark-mode' class on body every time darkMode boolean changes 
+  useEffect(() => {
+    document.body.classList.toggle('dark-mode', darkMode);
+  }, [darkMode]);
+
+  return [darkMode, setDarkMode];
+};
